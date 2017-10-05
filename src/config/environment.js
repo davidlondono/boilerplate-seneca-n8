3358e3955e78d2b1381a6ee5437ee2128482cdfe
David Londoño <dalon@Davids-MacBook-Pro.local>
2017-10-05 15:10:34 -0500

    config fix

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 let environment;
 let path;
 const env = '.env';
-const service = process.env.SERVICE;
-const platform = process.env.PLATFORM;
 
 switch (process.env.NODE_ENV) {
   case 'develop':
