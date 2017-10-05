ec1d48f801536b0add048dfcb327a674486e27c2
David Londoño <dalon@Davids-MacBook-Pro.local>
2017-10-05 15:06:09 -0500

    no error handling

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,21 +2,13 @@
 const _ = require('lodash');
 const handlers = require('./handlers');
 
-const errorParser = response => (err) => {
-  if (err.code === 100) {
-    response(null, { error: err.message, ok: false });
-  } else {
-    response(err);
-  }
-};
 
 const handlerAdder = seneca => (item) => {
   seneca.add(item.pattern, (msg, response) => {
     const successHandler = data => response(null, data);
-    const errorHandler = errorParser(response);
     Promise.resolve(item.handler(msg, successHandler))
       .then(() => undefined)
-      .catch(errorHandler);
+      .catch(err => response(err));
   });
 };
 
